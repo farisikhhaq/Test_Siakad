030ea46516cde987509ae3c0b33ecb76d8b0984c
usernamefarisikhhaq <farisikhlasulhaq@yahoo.co.id>
2021-03-25 10:36:35 +0700

    Jobsheet 7 ORM (Update Model)

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+class Mahasiswa extends Model 
+{
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'id_mahasiswa';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+    ];
+};
