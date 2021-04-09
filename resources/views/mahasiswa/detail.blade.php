58846cff701d272c386b029228ead0e0c19eadd4
usernamefarisikhhaq <farisikhlasulhaq@yahoo.co.id>
2021-04-09 23:43:40 +0700

    Jobsheet 9 (Praktikum 1 - Show Blade)

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -12,7 +12,7 @@
                         <li class="list-group-item"><b>Nim: </b>{{ $Mahasiswa->nim }}</li>
                         <li class="list-group-item"><b>Nama: </b>{{ $Mahasiswa->nama }}</li>
                         <li class="list-group-item"><b>E-mail: </b>{{$Mahasiswa->email}}</li>
-                        <li class="list-group-item"><b>Kelas: </b>{{ $Mahasiswa->kelas }}</li>
+                        <li class="list-group-item"><b>Kelas: </b>{{ $Mahasiswa->kelas->nama_kelas }}</li>
                         <li class="list-group-item"><b>Jurusan: </b>{{ $Mahasiswa->jurusan }}</li>
                         <li class="list-group-item"><b>E-mail: </b>{{$Mahasiswa->alamat}}</li>
                         <li class="list-group-item"><b>Tanggal Lahir: </b>{{$Mahasiswa->tgl_lahir}}</li>
