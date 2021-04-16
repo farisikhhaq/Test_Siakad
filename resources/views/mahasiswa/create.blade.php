f3d08b0ee9d0267bf591b05ddd0754cdd04562f3
usernamefarisikhhaq <farisikhlasulhaq@yahoo.co.id>
2021-04-16 10:01:40 +0700

    Jobsheet 10 (Tugas : Create view add photo)

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -20,15 +20,19 @@
                             </ul>
                         </div>
                     @endif
-                    <form method="post" action="{{ route('mahasiswa.store') }}" id="myForm">
+                    <form method="post" action="{{ route('mahasiswa.store') }}" id="myForm" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label for="Nim">Nim</label>
-                            <input type="text" name="nim" class="form-control" id="nim" aria-describedby="nim">
+                            <input type="text" name="nim" class="form-control" id="nim" aria-describedby="nim" value="{{old('nim')}}">
                         </div>
                         <div class="form-group">
                             <label for="Nama">Nama</label>
                             <input type="Nama" name="nama" class="form-control" id="nama" ariadescribedby="nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="Foto">Foto</label>
+                            <input type="file" name="foto" class="form-control-file" id="Foto" aria-describedby="Foto" value="{{old('foto')}}">
                         </div>
                         <div class="form-group">
                             <label for="email">E-mail</label>
@@ -42,21 +46,21 @@
                             <label for="kelas">Kelas</label>
                             <select class="form-control" id="kelas" name="kelas">
                                 @foreach ($class as $kls)
-                                    <option value="{{$kls->id}}">{{$kls->nama_kelas}}</option>
+                                <option value="{{$kls->id}}" {{old('kelas') == $kls->id ? 'selected' : ''}}>{{$kls->nama_kelas}}</option>
                                 @endforeach
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="jurusan">Jurusan</label>
-                            <input type="jurusan" name="jurusan" class="form-control" id="jurusan" ariadescribedby="jurusan">
+                            <input type="text" name="jurusan" class="form-control" id="jurusan" aria-describedby="jurusan" value="{{old('jurusan')}}">
                         </div>
                         <div class="form-group">
-                            <label for="jurusan">Alamat</label>
-                            <input type="jurusan" name="alamat" class="form-control" id="alamat" ariadescribedby="alamat">
+                            <label for="alamat">Alamat</label>
+                            <input type="alamat" name="alamat" class="form-control" id="alamat" ariadescribedby="alamat" value="{{old('alamat')}}">
                         </div>
                         <div class="form-group">
                             <label for="tgl_lahir">Tanggal Lahir</label>
-                            <input type="date" name="tgl_lahir" class="form-control" id="tgl_lahir" ariadescribedby="tgl_lahir" >
+                            <input type="date" name="tgl_lahir" class="form-control" id="tgl_lahir" ariadescribedby="tgl_lahir"  value="{{old('tgl_lahir')}}">
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
